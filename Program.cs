f17988c60f948e2427e4905712a5a20cf2363223
KnightGo <15001127165@163.com>
2018-10-21 12:33:22 +0800

    new Console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace www_gittest_tortoise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
