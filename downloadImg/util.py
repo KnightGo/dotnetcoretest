545ce75f21fbf6fb97a75a233daa5fc51a7cfbc9
wangly <2627228152@163.com>
2020-08-22 23:31:27 +0800

    update

--- downloadImg/util.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def saveimg(img_byte,code,filePath):
-    filename = filePath + code+".png"
-    with open(filename, "wb") as f:
-            f.write(img_byte) # 将内容写入图片
-                  
-
-
